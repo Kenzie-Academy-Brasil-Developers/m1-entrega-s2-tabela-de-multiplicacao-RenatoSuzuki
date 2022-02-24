c4eb6073e43fc0a12c4f7a28043c5d1b90603928
PandaSuzuki <renatosuzuki03@gmail.com>
2022-02-24 16:55:07 -0300

    S2B31

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+function table(){
 let array = []
-for (let i = 0; i < 10; i++){
+for (let i = 0; i <= 10; i++){
     array[i]=[]
-    for(let j = 0; j <10; j++){
+    for(let j = 0; j <= 10; j++){
         array[i].push([i]*[j])
     }
+  }
+  return array
 }
-console.table(array)
+console.table(table())
+
+//bonus
+
+function tabelaMulti(array = 11){
+    let tabela = []
+    for (let i = 0; i < array; i++){
+        tabela[i]=[]
+        for(let j = 0; j < array; j++){
+            tabela[i].push([i]*[j])
+        }
+    }
+    return tabela
+}
+console.table(tabelaMulti())
